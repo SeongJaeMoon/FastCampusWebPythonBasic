ae990ad7eea7f22395165e635d0d7067748835a4
SeongjaeMoon <seongjae.m@gmail.com>
2018-09-08 22:03:59 +0900

    fixed typo

--- Learning/Python/Course07/task/main.py
+++ Learning/Python/Course07/task/main.py
@@ -20,7 +20,7 @@ def insert_sort(num_list):
 
 num_list = sample(range(1, 10001), 10000) # 랜덤한 순서로 1 ~ 100 까지 샘플 리스트 100개 생성
 print("sample:", num_list) # 랜덤한 리스트 출력
-# 삽입 정렬 실행 시간 -> Insertion sort를 통해 정렬하는 데 걸린 시간)
+# 삽입 정렬 실행 시간 -> (Insertion sort 함수를를 통해 입력 값(num_list)을 정렬하는 데 걸린 시간)
 run_time(insert_sort(num_list))
 
 
--- Learning/Python/Course08/README.md
+++ Learning/Python/Course08/README.md
@@ -1,1 +1,3 @@
 # Python 예외 처리
+키워드 : [Python 예외](https://docs.python.org/3.7/reference/executionmodel.html#exceptions), [Python 예외 처리](https://docs.python.org/3.7/reference/compound_stmts.html#try), [Python 예외 발생](https://docs.python.org/3.7/reference/simple_stmts.html#raise)
+
