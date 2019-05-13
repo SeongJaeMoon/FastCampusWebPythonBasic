b60aad140f5153b3f946eefb1e04ac214aebf209
SeongjaeMoon <seongjae.m@gmail.com>
2019-05-13 10:35:52 +0900

    fixed code error

--- Learning/Python/Course07/task/main.py
+++ Learning/Python/Course07/task/main.py
@@ -12,13 +12,13 @@ def insert_sort(num_list):
 	for i in range(1, len(num_list)):
 		j = i - 1
 		key = num_list[i]
-		while num_list[j] > key and j > 0:
+		while num_list[j] > key and j >= 0:
 			num_list[j+1] = num_list[j]
 			j = j - 1
 		num_list[j+1] = key
 
 
-num_list = sample(range(1, 10001), 10000) # 랜덤한 순서로 1 ~ 100 까지 샘플 리스트 100개 생성
+num_list = sample(range(1, 10001), 10000) # 랜덤한 순서로 1 ~ 10000 까지 샘플 리스트 100개 생성
 print("sample:", num_list) # 랜덤한 리스트 출력
 # 삽입 정렬 실행 시간 -> (Insertion sort 함수를를 통해 입력 값(num_list)을 정렬하는 데 걸린 시간)
 run_time(insert_sort(num_list))
